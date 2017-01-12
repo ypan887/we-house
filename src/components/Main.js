6486eb7c005ab15a7087c34d9919e31786cd8b98
ypan887 <ypan887@gmail.com>
2017-01-12 10:57:28 -0800

    added button that descending sort apartment list based on popularity or price

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 require('normalize.css/normalize.css');
+require('bootstrap/dist/css/bootstrap.css');
 require('styles/App.css');
 
 import React, { Component } from 'react';
@@ -18,18 +19,65 @@ apartmentDatas = ((apartmentsArr) => {
 
 
 class AppComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      order: "popularity"
+    }
+    this.pop = this.pop.bind(this);
+    this.price = this.price.bind(this);
+    this.sortApt = this.sortApt.bind(this);
+  }
+
+  sortApt(sortValue){
+    apartmentDatas = apartmentDatas.sort((a, b) => {
+      return b[sortValue] - a[sortValue];
+    });
+  }
+
+  pop(){
+    this.setState({order: "popularity"});
+  }
+
+  price(){
+    this.setState({order: "price"});
+  }
+
   render() {
-    let apartments = [];
+    let apartments = [],
+        popButtonName = "btn",
+        priceButtonName = "btn";
+    if (this.state.order==="price") {
+      this.sortApt(this.state.order);
+      priceButtonName += " active";
+    } else {
+      this.sortApt(this.state.order);
+      popButtonName += " active";
+    }
+
     apartmentDatas.forEach((value, index) => {
       apartments.push(<Apartment data={ value } key={ index } />)
       });
 
     return (
       <div className="index">
-        <div className="content">
-          <section className="apartment-list">
-            { apartments }
-          </section>
+        <div className="container">
+          <div className="content">
+            <div className="row">
+              <div className="col-sm-10 col-sm-offset-1">
+                <div>
+                  <h2 className="title">MN: { apartmentDatas.length } Apartments</h2>
+                </div>
+                <div className="sort-buttons">
+                  <button className={ popButtonName } onClick={ this.pop }>popularity</button>
+                  <button className={ priceButtonName } onClick={ this.price }>Price</button>
+                </div>
+                <section className="apartment-list">
+                  { apartments }
+                </section>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
--- src/components/apartment.js
+++ src/components/apartment.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react';
 class apartmentComponent extends Component {
   render() {
     return(
-      <div>
-        <div className="row">
-          <div className="col-sm-5">
+      <div className="row apartment">
+        <div className="col-sm-5 col">
+          <div className="list-image">
             <figure>
               <img
                 src={ this.props.data.imageURL }
@@ -13,19 +13,21 @@ class apartmentComponent extends Component {
               />
             </figure>
           </div>
-          <div className="col-sm-7">
+        </div>
+        <div className="col-sm-7 col">
+          <div className="list-content">
             <div className="list-title">
-              <h2>{ this.props.data.title }</h2>
+              <h2>{ this.props.data.name }</h2>
             </div>
             <div className="list-desc">
               <p>{ this.props.data.description }</p>
             </div>
-            <div>
-              <span className="price-prefix">From</span>
-              <span className="price-value">${ this.props.data.price }</span>
-            </div>
-            <div>
-              <span className="price-unit">USD/Month</span>
+            <div className="price">
+              <div>
+                <span className="price-prefix">From </span>
+                <span className="price-value">${ this.props.data.price }</span>
+              </div>
+              <div className="price-unit">USD / Month</div>
             </div>
           </div>
         </div>
